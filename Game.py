507934d514e9f799c115acac926a15806374d2f9
CRehfuss <crehfuss@umich.edu>
2015-02-07 13:59:34 -0500

    Header File
    I made a file for the actual game, you’ll have to upload it to your own
    thing. I’m thinking maybe we should make Wall and Player subclasses of
    an Image class though.

---
+++ Game.py
@@ -0,0 +1,18 @@
+#Switch it Up
+#Main file
+
+
+import pygame
+from pygame.locals import *
+
+pygame.init()
+
+screen = pygame.display.set_mode([700,500])
+pygame.display.set_caption("Switch It Up")
+
+#The Implementation of Player class should follow
+class Player(pygame.sprite.Sprite):
+
+#The Implementation of Wall class should follow
+class Wall(pygame.sprite.Sprite):
+    
